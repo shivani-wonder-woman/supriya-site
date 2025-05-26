0234bba52e3ed36ce84f4c17d46f781a448a7eae
Shivani Kumari <shivani.shivi333@gmail.com>
2025-05-26 22:06:37 +0530

    layout completed

---
+++ src/app/Videos/page.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import styles from "./video.module.css";
+const VideoPage: FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>videos will be coming soon</h1>
+    </div>
+  );
+};
+export default VideoPage;
